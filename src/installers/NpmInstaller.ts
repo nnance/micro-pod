27aeea7f33f3beec51df500a72ee041ed52386ee
Nick Nance <nance.nick@gmail.com>
2017-01-15 22:53:37 -0800

    added get-port to installer

--- src/installers/NpmInstaller.ts
+++ src/installers/NpmInstaller.ts
@@ -1,10 +1,11 @@
+import * as getPort from 'get-port'
 import {IService, IServiceInstaller, Port, ResultCode} from '../types'
 
 export class NpmInstaller implements IServiceInstaller {
   public installService(service: IService): Promise<Port> {
-    return new Promise((resolve, reject) => {
-      console.log(`installed service ${service.name}`)
-      resolve('3000')
+    return new Promise(async (resolve, reject) => {
+      const port = await getPort()
+      console.log(`installed service ${service.name} at ${port}`)
     })
   }
 
